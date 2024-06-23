52947da53f3d29f2e191ae609d5198a87a4af340
Alexander Stepchenko <x.GeoCHiP.x@yandex.ru>
2024-06-23 22:58:53 +0300

    refactor: clean up main.cpp

--- cmilan/src/main.cpp
+++ cmilan/src/main.cpp
@@ -1,30 +1,27 @@
-#include "parser.h"
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
-void printHelp()
-{
-	std::cout << "Usage: cmilan input_file" << std::endl;
+#include "parser.h"
+
+void PrintHelp() {
+    std::cout << "Usage: cmilan input_file" << std::endl;
 }
 
-int main(int argc, char** argv)
-{
-	if(argc < 2) {
-		printHelp();
-		return EXIT_FAILURE;
-	}
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        PrintHelp();
+        return EXIT_FAILURE;
+    }
 
-	std::ifstream input;
-        input.open(argv[1]);
+    std::ifstream input;
+    input.open(argv[1]);
 
-	if(input) {
-		Parser p(argv[1], input);
-		p.Parse();
-		return EXIT_SUCCESS;
-	}
-	else {
-		std::cerr << "File '" << argv[1] << "' not found" << std::endl;
-		return EXIT_FAILURE;
-	}
+    if (input) {
+        Parser p(argv[1], input);
+        p.Parse();
+        return EXIT_SUCCESS;
+    } else {
+        std::cerr << "File '" << argv[1] << "' not found" << std::endl;
+        return EXIT_FAILURE;
+    }
 }
-
